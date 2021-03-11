574a8ce64128f6c60bbc3b029811696378f1348e
user <user@DESKTOP-CEJKNMA>
2021-03-10 23:11:57 -0600

    primer commit

---
+++ src/ejercicio/pkg1/Ejercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    System.out.println("Hola, soy de la clase programación orentada a objetos\n" );
+
+  
+
+    }
+    
+}
